870bd29b8d40a1e65404e675fcea900dba06e7aa
PrimaSentana <premasentana123@gmail.com>
2026-05-17 19:24:18 +0800

    migrate to dotenv file

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,16 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:prima@localhost:5432/savebite"
-    SECRET_KEY: str = "3451c5993b4c6c48c69427b883faab766eb8507a3325a7058977ab7c7cc8ff2c"
+    DATABASE_URL: str
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    CLOUDINARY_CLOUD_NAME: str
+    CLOUDINARY_API_KEY: int
+    CLOUDINARY_API_SECRET: str
+    
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
