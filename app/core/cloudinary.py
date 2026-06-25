f64de86c5f5cd869bf643d81eee934a68f346b67
PrimaSentana <premasentana123@gmail.com>
2026-06-25 14:16:13 +0800

    add upload function for merchants' banner and logo, and also menu's image

--- app/core/cloudinary.py
+++ app/core/cloudinary.py
@@ -23,5 +23,71 @@ async def upload_profile_photo(file_bytes: bytes, user_id: int) -> str:
     
     return result["secure_url"]
 
+async def upload_profile_photo_merchant(file_bytes: bytes, merchant_id: int) -> str:
+    result = cloudinary.uploader.upload(
+        file_bytes, 
+        folder = "profile_photos-merchant",
+        public_id = f"merchant_{merchant_id}",
+        overwrite = True,
+        transformation = [
+            {"width": 400, "height": 400, "crop": "fill"},
+            {"quality": "auto"},
+            {"format": "webp"}
+        ]
+    )
+    
+    return result["secure_url"]
+
+async def upload_banner_photo_merchant(file_bytes: bytes, merchant_id: int) -> str:
+    result = cloudinary.uploader.upload(
+        file_bytes, 
+        folder = "banner_photos-merchant",
+        public_id = f"merchant_{merchant_id}",
+        overwrite = True,
+        transformation = [
+            {"quality": "auto"},
+            {"format": "webp"}
+        ]
+    )
+    
+    return result["secure_url"]
+
+# async def upload_photo_menu(file_bytes: bytes, merchant_id: int) -> str:
+#     result = cloudinary.uploader.upload(
+#         file_bytes, 
+#         folder = "menu_photos-merchant",
+#         public_id = f"merchant_{merchant_id}",
+#         overwrite = True,
+#         transformation = [
+#             {"quality": "auto"},
+#             {"format": "webp"}
+#         ]
+#     )
+    
+#     return result["secure_url"]
+
+async def upload_menu_image(file_bytes: bytes, menu_id: int) -> str:
+    result = cloudinary.uploader.upload(
+        file_bytes,
+        folder="menu_images",
+        public_id=f"menu_{menu_id}",
+        overwrite=True,
+        transformation=[
+            {"width": 800, "height": 600, "crop": "fill"},
+            {"quality": "auto"},
+            {"format": "webp"}
+        ]
+    )
+    return result["secure_url"]
+
 async def delete_profile_photo(user_id: int):
     cloudinary.uploader.destroy(f"profile_photos/user_{user_id}")
+    
+async def delete_merchant_logo(merchant_id: int):
+    cloudinary.uploader.destroy(f"profile_photos-merchant/merchant_{merchant_id}")
+    
+async def delete_merchant_banner(merchant_id: int):
+    cloudinary.uploader.destroy(f"banner_photos-merchant/merchant_{merchant_id}")
+
+async def delete_menu_image(menu_id: int):
+    cloudinary.uploader.destroy(f"menu_images/menu_{menu_id}")
